b29b489af8f9d78f20449b1c774ee327389132d6
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2023-05-26 06:31:41 +0000

    Update cors

--- api/cors.js
+++ api/cors.js
@@ -1,6 +1,19 @@
 export default async function handler(request, response) {
-  
   const https = require('https');
+
+  // new
+  const { headers } = request;
+  
+  // 檢查請求來源是否為本地主機（localhost）
+  const origin = headers.origin || '';
+  if (origin.includes('localhost')) {
+    // 允許來自本地主機的跨域請求
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Methods', 'GET');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  }
+  // 
+  
   let query = Object.entries(request.query);
   query.shift();
   let url = request.query.url;
@@ -8,13 +21,11 @@ export default async function handler(request, response) {
     url += '&' + entry[0] + '=' + entry[1];
   });
   
-  
-  const {status,data} = await getRequest(url);
+  const { status, data } = await getRequest(url);
   
   response.status(status).send(data);
 
   function getRequest(url) {
-    
     return new Promise(resolve => {
       const req = https.get(url, (resp) => {
         let data = '';
